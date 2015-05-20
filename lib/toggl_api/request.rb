1ebefe22dc58823ed04c9122837bc4e9cedf842d
Joseph Krump <krump.joseph@gmail.com>
2015-05-20 15:28:04 -0700

    logging exception.inspect for parse exception

--- lib/toggl_api/request.rb
+++ lib/toggl_api/request.rb
@@ -60,10 +60,7 @@ module Toggl
         data = mash(MultiJson.load(response.body))
         (data.is_a?(Hash) && data.key?('data')) ? data['data'] : data
       rescue MultiJson::ParseError => exception
-        Rails.logger.error exception.data
-        Rails.logger.error exception.cause
-        Rails.logger.debug exception.data
-        Rails.logger.debug exception.cause
+        Rails.logger.error exception.inspect
       end
     end
 
